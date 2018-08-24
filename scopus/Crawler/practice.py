d08fab35dca4ee80a9a8f83e4d269b98f118780f
soonki.jung <soonki.jung@gmail.com>
2018-08-24 21:05:09 +0900

    practice001

---
+++ scopus/Crawler/practice.py
@@ -0,0 +1,31 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def spider(max_pages):
+    page = 1
+    while page < max_pages:
+        url = 'http://creativeworks.tistory.com/' + str(page)
+        source_code = requests.get(url)
+        plain_test = source_code.text
+        soup = BeautifulSoup(plain_test, 'lxml')
+        for link in soup.select('h2 > a'):
+            href = "http://creativeworks.tistory.com" + link.get('href')
+            title = link.string
+            print(href)
+            print(title)
+            get_single_article(href)
+
+        page += 1
+
+
+def get_single_article(item_url):
+    source_code = requests.get(item_url)
+    plain_text = source_code.text
+    soup = BeautifulSoup(plain_text, 'lxml')
+
+    for contents in soup.select('p > span'):
+        print(contents.text)
+
+
+spider(10)
